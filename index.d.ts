32e9ef1d2890ecbd5f046eedca92136ed7d6f874
Nicole Cordes <ichhabrecht+github@cordes.co>
2022-03-31 13:05:38 +0200

    Update @types/node dependency

--- index.d.ts
+++ index.d.ts
@@ -1,13 +1,16 @@
-// Type definitions for portastic v1.0.1
+// Type definitions for portastic
 // Project: portastic
 // Definitions by: James Booth github.com/jabooth
 
-export function test(port: number, interface?: string, callback?: Function): Promise<boolean>
+export function test(port: number, iface?: string, callback?: Function): Promise<boolean>
 
-export function find(options: {min?: number, max?: number, retrieve?: number},
-                     interface?: string, callback?: Function): Promise<number[]>
+export function find(
+    options: {min?: number, max?: number, retrieve?: number},
+    iface?: string,
+    callback?: Function
+): Promise<number[]>
 
-export function filter(ports: number[], interface?: string, callback?: Function): Promise<number[]>
+export function filter(ports: number[], iface?: string, callback?: Function): Promise<number[]>
 
 import { EventEmitter } from 'events'
 export class Monitor extends EventEmitter {
